a9e6c32fe863c813d3c1adff5fc2ce485bcc791c
Jędrzej Drygas <170216@student.ue.wroc.pl>
2018-10-19 23:03:19 +0200

    Add project files.

---
+++ ConjugationClasroom/ConjugationClasroom/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ConjugationClasroom.Models;
+
+namespace ConjugationClasroom.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
